070a408486bd9595a988e780b70e4e6e8f10a69a
Michael Boerman <michaelboerman@hey.com>
2022-03-18 14:48:44 -0400

    touch up code to reduce duplication

--- app.R
+++ app.R
@@ -10,22 +10,15 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
 
-  user_login <- reactive({
-    if (!is.null(session$user)) {
-      session$user
-    } else {
-      Sys.getenv('USER')
-    }
-  })
-  output$user_login <- renderText(paste0("The user's computer name is: ", user_login(), "."))
+  if (!is.null(session$user)) {
+    user_login   <- reactive(session$user)
+    login_source <- reactive("from session$user")
+  } else {
+    user_login   <- reactive(Sys.getenv('USER'))
+    login_source <- reactive("from 'Sys.getenv('USER')") 
+  }
 
-  login_source <- reactive({
-    if (!is.null(session$user)) {
-      "From 'session$user'"
-    } else {
-      "From 'Sys.getenv('USER')'"
-    }
-  })
+  output$user_login   <- renderText(paste0("The user's computer name is: ", user_login(), "."))
   output$login_source <- renderText(paste0("The login source is: ", login_source(), "."))
 }
 
